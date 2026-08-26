154fc3d64113a3793a3f0e1d4e6f08376818adfa
Farhan <farhanbaji111@gmail.com>
2026-08-26 23:58:43 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,60 @@
+#day1 starts with basic print() statements
+print("Hello, World!")
+print("My name is Mohammed Farhan")
+print("I am learning python Programming language")
+
+
+#using print() to print multiple types of data
+print(5)
+print(3.14)
+print(True)
+print(False)
+
+
+
+#using different features of print() function like sep, end
+
+#use of sep='/'
+print("hey there","how are you","iam Mohammed Farhan","nice to  meet you",sep='/')
+
+
+#use of sep='/' in different data types
+print('Day1-200',5, 3.14, True, False, sep='/')
+
+#use of sep='-' in print() function
+print('Day1-200',5, 3.14, True, False, sep='-')
+
+
+#use of end='' in print() function
+print("hey there",end=' ')
+print("how are you",end=' ')
+print("iam Mohammed Farhan",end=' ')
+print("nice to meet you")
+
+#use of end='spiderman' in print() function
+print("hey there",end=' spiderman ')
+print("how are you",end=' spiderman ')
+print("iam Mohammed Farhan",end=' spiderman ')
+print("nice to meet you",end=' spiderman ')
+
+
+
+
+
+#now let us learn excape sequences in python
+#using \n to print in new line
+print("hey there\nhow are you\niam Mohammed Farhan\nnice to meet you")
+
+#using \t to print in tab space
+print("hey there\thow are you\tiam Mohammed Farhan\tnice to meet you")
+
+#using \\ to print backslash
+print("hey there\\how are you\\iam Mohammed Farhan\\nice to meet you")
+
+#using \' to print single quote
+print('hey there\'how are you\'iam Mohammed Farhan\'nice to meet you')
+
+#using \" to print double quote
+print("hey there\"how are you\"iam Mohammed Farhan\"nice to meet you")
+
+
