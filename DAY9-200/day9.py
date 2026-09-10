0bb6e319b1a84333a34d56befffd64a419444fae
Farhan <farhanbaji111@gmail.com>
2026-09-10 21:51:06 +0530

    simple practice

--- DAY9-200/day9.py
+++ DAY9-200/day9.py
@@ -1,3 +1,5 @@
-#DAY 9 :
+#DAY 9 :String Deep Dive: indexing,slicing ,methods
 
-print("hello world!")
+#strings : string is the sequence of character
+name = "farhan"
+print(f"the varible name contains data {name} in it and it is of type {type(name)}")
